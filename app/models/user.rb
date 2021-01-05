b18cdad0b63a9e965bc3aca5e9bc798ae5dfbf7e
Amanatsu90 <s90.amano.natsuki@gmail.com>
2021-01-05 18:16:49 +0900

    正規表現の設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :kana_last, presence: true
-  validates :kana_first, presence: true
   validates :birth, presence: true
+  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
+  
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カタカナを使用してください' } do
+    validates :kana_last
+    validates :kana_first
+  end
 
 end

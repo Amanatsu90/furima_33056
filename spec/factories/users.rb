c134628b7ef9071b9e39d2579a97c3c6cb638831
Amanatsu90 <s90.amano.natsuki@gmail.com>
2021-01-05 19:58:18 +0900

    Rubocopの実行、コメント削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials}
-    last_name             {"スズキ"}
-    first_name            {"ハナコ"}
-    kana_last             {"スズキ"}
-    kana_first            {"ハナコ"}
-    birth                 {"1930-1-1"}
-    email                 {Faker::Internet.free_email}
+    nickname              { Faker::Name.initials }
+    last_name             { 'スズキ' }
+    first_name            { 'ハナコ' }
+    kana_last             { 'スズキ' }
+    kana_first            { 'ハナコ' }
+    birth                 { '1930-1-1' }
+    email                 { Faker::Internet.free_email }
     password = Faker::Internet.password(min_length: 6)
-    password {password}
-    password_confirmation {password}
+    password { password }
+    password_confirmation { password }
   end
 end

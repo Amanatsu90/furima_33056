b20dcde211fdfe7c91b07a52f4bf1c075e79330f
Amanatsu90 <s90.amano.natsuki@gmail.com>
2021-01-12 21:06:13 +0900

    購入情報の登録機能を実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,26 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
+    @order_shipping = OrderShipping.new
   end
-
-  def new
-  end
-
+  
   def create
+    @order_shipping = OrderShipping.new(order_params)
+    if @order_shipping.valid?
+      @order_shipping.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order_shipping).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -4,13 +4,4 @@ class ShippingAddress < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
 
-  with_options presence: true do
-    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ },
-    validates :prefecture_id
-    validates :city
-    validates :addresses
-    validates :phone_number, length: {maximum: 11}
-  end
-
-  validates :prefecture_id, numericality: { other_than: 1 }
 end
